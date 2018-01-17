928d156ab696c393d1c73a9f7395a335d5d59db1
Irit Goihman <igoihman@redhat.com>
2018-01-17 12:36:13 +0200

    remove all ovs related tasks

--- filter_plugins/ovs-dpdk.py
+++
@@ -1,8 +0,0 @@
-from __future__ import (absolute_import, division, print_function)
-
-
-def get_core_mask(cores):
-    mask = 0
-    for core in cores:
-        mask |= (1 << int(core))
-    return hex(mask)
